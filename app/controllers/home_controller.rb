95492b0af6b2fbe49342308c8c6b256657c247d5
Fabricio Raphael Baliza <fabricioraphael.b@gmail.com>
2011-11-06 00:19:38 -0200

    foi?

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,14 +8,15 @@ class HomeController < ApplicationController
     @home = Home.new
     @arq_final = Array.new
   
-    @arq = params[:home][:arq]
+    @arq = params[:arq][:idarq]
+    
     @arq_name = @arq.original_filename
     
     @home.save_file @arq
+        
+        @arq_final = @home.read_arq params[:home][:value].to_i, params[:home][:operacao], @arq_name
     
-    @arq_final = @home.read_arq params[:home][:value].to_i, params[:home][:operacao], @arq_name
-
-    @home.write_arq @arq_final, @arq_name
+        @home.write_arq @arq_final, @arq_name
         
     render :index, :arq_name => @arq_name
   end
