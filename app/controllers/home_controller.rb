19f5cfb52cc10ffa40727eba3a4b851cb69bc299
Fabricio Raphael Baliza <fabricioraphael.b@gmail.com>
2011-11-08 00:20:44 -0200

    gogo

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,7 +8,7 @@ class HomeController < ApplicationController
     @home = Home.new
     @arq_final = Array.new
   
-    @arq = params[:arq]
+    @arq = params[:arq][:id]
     
     puts "=========================== NIL? #{@arq.nil?}"
     puts "=========================== WTF? #{@arq}"
