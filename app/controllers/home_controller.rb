9d3cbfb1bddc9fecdb0291b84eeadb226b68de55
Fabricio Raphael Baliza <fabricioraphael.b@gmail.com>
2011-11-08 00:15:28 -0200

    gogo

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,13 +10,18 @@ class HomeController < ApplicationController
   
     @arq = params[:arq][:idarq]
     
+    puts "=========================== NIL? #{@arq.nil?}"
+    puts "=========================== WTF? #{@arq}"
+    puts "=========================== class #{@arq.class}"
+    puts "=========================== name: #{@arq.original_filename}"    
     @arq_name = @arq.original_filename
-    
+    puts "=========================== NIL2? #{@arq_name.nil?}"
+    puts "=========================== class2 #{@arq_name.class}"
     @home.save_file @arq
         
-        @arq_final = @home.read_arq params[:home][:value].to_i, params[:home][:operacao], @arq_name
-    
-        @home.write_arq @arq_final, @arq_name
+    @arq_final = @home.read_arq params[:home][:value].to_i, params[:home][:operacao], @arq_name
+
+    @home.write_arq @arq_final, @arq_name
         
     render :index, :arq_name => @arq_name
   end
